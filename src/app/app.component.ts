fb1778a3742defbf49820ded21a5f9996f727d5c
Florian <13017799@student.hhs.nl>
2019-12-26 19:17:50 +0100

    Cart added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'shopping';
+  results = [];
+  products = [];
+  cart = [];
+  
+  addToCart = function(item) {
+    alert(item.name + " was added to your cart");
+    this.cart.push(item);
+  }
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() : void {
+    interface Product {
+      name : string;
+      category: string;
+      price: number;
+    }
+    this.http.get<Product>('https://localhost:44300/api/Products/').subscribe(data => {
+      for (let i in data) {
+        this.results.push(data[i]);
+      }
+    });
+  }
 }
